441e8eab3b24bfa0a7c678d38547f4df36efc0e9
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-12-20 19:45:11 +0100

    added cleanup files. Seperated wordpress and class cleanup

---
+++ inc/cleanup-classes.php
@@ -0,0 +1,153 @@
+<?php
+//
+// Cleanup | Classes
+//
+// @package      reef-theme
+// @author       Daniël R.
+// @since        1.0.0
+//
+
+
+
+//
+// Clean body classes
+//
+function reef_clean_body_classes( $classes ) {
+
+	$allowed_classes = [
+		'singular',
+		'single',
+		'page',
+		'archive',
+		'admin-bar',
+		'full-width-content',
+		'content-sidebar',
+		'content',
+	];
+
+	return array_intersect( $classes, $allowed_classes );
+
+}
+add_filter( 'body_class', 'reef_clean_body_classes', 20 );
+
+
+
+//
+// Clean menu classes
+//
+function reef_clean_nav_menu_classes( $classes ) {
+	if( ! is_array( $classes ) )
+		return $classes;
+
+	foreach( $classes as $i => $class ) {
+
+		// Remove class with menu item id
+		$id = strtok( $class, 'menu-item-' );
+		if( 0 < intval( $id ) )
+			unset( $classes[ $i ] );
+
+		// Remove menu-item-type-*
+		if( false !== strpos( $class, 'menu-item-type-' ) )
+			unset( $classes[ $i ] );
+
+		// Remove menu-item-object-*
+		if( false !== strpos( $class, 'menu-item-object-' ) )
+			unset( $classes[ $i ] );
+
+		// Change page ancestor to menu ancestor
+		if( 'current-page-ancestor' == $class ) {
+			$classes[] = 'current-menu-ancestor';
+			unset( $classes[ $i ] );
+		}
+	}
+
+	// Remove submenu class if depth is limited
+	if( isset( $args->depth ) && 1 === $args->depth ) {
+		$classes = array_diff( $classes, array( 'menu-item-has-children' ) );
+	}
+
+	return $classes;
+}
+add_filter( 'nav_menu_css_class', 'reef_clean_nav_menu_classes', 5 );
+
+
+//
+// Clean Post classes
+//
+function reef_clean_post_classes( $classes ) {
+
+	if( ! is_array( $classes ) )
+		return $classes;
+
+	$allowed_classes = array(
+  		'hentry',
+  		'type-' . get_post_type(),
+   	);
+
+	return array_intersect( $classes, $allowed_classes );
+}
+add_filter( 'post_class', 'reef_clean_post_classes', 5 );
+
+
+
+//
+// Clean archive prefix
+//
+function reef_archive_title_remove_prefix( $title ) {
+	$title_pieces = explode( ': ', $title );
+	if( count( $title_pieces ) > 1 ) {
+		unset( $title_pieces[0] );
+		$title = join( ': ', $title_pieces );
+	}
+	return $title;
+}
+add_filter( 'get_the_archive_title', 'reef_archive_title_remove_prefix' );
+
+
+
+//
+// Staff comment classes
+//
+function reef_staff_comment_class( $classes, $class, $comment_id, $comment, $post_id ) {
+	if( empty( $comment->user_id ) )
+		return $classes;
+	$staff_roles = array( 'comment_manager', 'author', 'editor', 'administrator' );
+	$staff_roles = apply_filters( 'reef_staff_roles', $staff_roles );
+	$user = get_userdata( $comment->user_id );
+	if( !empty( array_intersect( $user->roles, $staff_roles ) ) )
+		$classes[] = 'staff';
+	return $classes;
+}
+add_filter( 'comment_class', 'reef_staff_comment_class', 10, 5 );
+
+
+
+//
+// Remove avatars
+//
+function reef_remove_avatars_from_comments( $avatar ) {
+	global $in_comment_loop;
+	return $in_comment_loop ? '' : $avatar;
+}
+add_filter( 'get_avatar', 'reef_remove_avatars_from_comments' );
+
+
+
+//
+// Clean form and button classes
+//
+function reef_comment_form_button_class( $args ) {
+	$args['class_submit'] = 'submit wp-block-button__link';
+	return $args;
+}
+add_filter( 'comment_form_defaults', 'reef_comment_form_button_class' );
+
+
+
+//
+// Excerpt more 
+//
+function reef_excerpt_more() {
+	return '&hellip;';
+}
+add_filter( 'excerpt_more', 'reef_excerpt_more' );
---
+++ inc/cleanup.php
@@ -0,0 +1,123 @@
+<?php
+//
+// Cleanup
+//
+// @package      reef-theme
+// @author       Daniël R.
+// @since        1.0.0
+//
+// TODO: Uitleg + links remove_actions en json api
+// https://gist.github.com/Auke1810/f2a4cf04f2c07c74a393a4b442f22267
+
+//
+// Shrink admin bar / expand on hover
+// https://css-tricks.com/snippets/wordpress/shrink-the-admin-bar-expand-on-hover/
+//
+add_action('get_header', 'my_filter_head');
+function my_filter_head() { 
+    remove_action('wp_head', '_admin_bar_bump_cb'); 
+}
+ 
+function my_admin_css() {
+        if ( is_user_logged_in() ) {
+        ?>
+        <style type="text/css">
+            #wpadminbar {
+                width: 35px;
+                min-width: auto;
+                overflow: hidden;
+                -webkit-transition: .2s width;
+                -webkit-transition-delay: 0s;
+                -moz-transition: .2s width;
+                -moz-transition-delay: 0s;
+                -o-transition: .2s width;
+                -o-transition-delay: 0s;
+                -ms-transition: .2s width;
+                -ms-transition-delay: 0s;
+                transition: .2s width;
+                transition-delay: 0s;
+            }
+            
+            #wpadminbar:hover {
+                width: 100%;
+                overflow: visible;
+                -webkit-transition-delay: 0;
+                -moz-transition-delay: 0;
+                -o-transition-delay: 0;
+                -ms-transition-delay: 0;
+                transition-delay: 0;
+            }
+        </style>
+        <?php }
+}
+add_action('wp_head', 'my_admin_css');
+
+
+
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+remove_action('wp_head', 'print_emoji_detection_script', 7 );
+remove_action('wp_print_styles', 'print_emoji_styles' );
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+//
+// Remove JSON API links in header html
+//
+function remove_json_api () {
+
+    // Remove the REST API lines from the HTML Header
+    remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+
+    // Remove the REST API endpoint.
+    remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+
+    // Turn off oEmbed auto discovery.
+    add_filter( 'embed_oembed_discover', '__return_false' );
+
+    // Don't filter oEmbed results.
+    remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+
+    // Remove oEmbed discovery links.
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+
+    // Remove oEmbed-specific JavaScript from the front-end and back-end.
+    remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+
+   // Remove all embeds rewrite rules.
+   add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+
+}
+add_action( 'after_setup_theme', 'remove_json_api' );
+
+//
+//	Snippet completely disable the REST API and shows {"code":"rest_disabled","message":"The REST API is disabled on this site."} 
+//	when visiting http://yoursite.com/wp-json/
+//
+function disable_json_api () {
+
+  // Filters for WP-API version 1.x
+  add_filter('json_enabled', '__return_false');
+  add_filter('json_jsonp_enabled', '__return_false');
+
+  // Filters for WP-API version 2.x
+  add_filter('rest_enabled', '__return_false');
+  add_filter('rest_jsonp_enabled', '__return_false');
+
+}
+add_action( 'after_setup_theme', 'disable_json_api' );
+
+
+
+
+?>
+
+
