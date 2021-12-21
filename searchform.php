6ac313b328d798611f146ceba8a61b79256470b5
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-12-21 22:49:36 +0100

    standaard searchform toegevoegd

---
+++ searchform.php
@@ -0,0 +1,18 @@
+<?php
+//
+// Searchform
+//
+// @package      reef-theme
+// @author       Daniël R.
+// @since        1.0.0
+// 
+// TODO: vertaling placeholder?
+?>
+
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text">Search for</span>
+		<input type="search" class="search-field" placeholder="Zoeken&hellip;" value="<?php echo get_search_query(); ?>" name="s" title="Search for" />
+	</label>
+	<button type="submit" class="search-submit"><?php echo wa_svg_icon( [ 'icon' => 'search' ] );?></button>
+</form>
