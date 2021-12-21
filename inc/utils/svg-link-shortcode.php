e73fc69cbfd66a8b5dceaec7359045a9852824c5
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-12-21 14:34:55 +0100

    size en class toegevoegd WIP

--- inc/utils/svg-link-shortcode.php
+++ inc/utils/svg-link-shortcode.php
@@ -4,12 +4,13 @@
 // Geupdate als shortcode met opties voor link en plaatje
 // Voorbeeld (alleen svg):  [wa_svg_icon icon=logo] 
 // Voorbeeld (svg en link): [wa_svg_icon icon=logo link=https://www.websiteaanbieder.nl]
-// Voorbeeld inline:  do_shortcode('[wa_svg_icon icon=shirt link=https://www.websiteaanbieder.nl]'); 
+// Voorbeeld inline:  wa_svg_icon(['icon' => 'logo' 'link' => 'https://www.websiteaanbieder.nl']); 
 //
 // !!! Vergeet niet het pad te controleren als de shortcode niet lijkt te werken !!!
 //
 // TODO: url check regex  
 // TODO: pad als argument
+// TODO: refactor 2 functies naar 1
 
 // Throw this in your theme and include it in your functions.php file 
 
@@ -29,29 +30,34 @@ function wa_svg_icon ($atts) {
   if ( is_array($atts) ) {
     extract(shortcode_atts(array(
     'icon' => '',
-    'link' => '',
+    'link' => NULL,
+    'class' => NULL,
+    'size' => NULL,
      ), $atts));
   }
 
   // Error geven als we geen plaatje hebben.
   if(!isset($atts['icon'])) {
-    return 'Icon error - Check Path or filename';
+    return 'Icon error - Check path or filename';
   }
 
   $path = get_stylesheet_directory() . '/assets/svg/' . $atts['icon'] . '.svg'; 
 
   // Checken of er een link aanwezig is, anders alleen het plaatje doorgeven.
   // Zonder check geeft  $atts['value'] een undefined array key error.
-  if(isset($atts['link'])) {
+  // if(isset($atts['link'])) {
     $args = [
-      'css_class' => 'icon icon-' . $atts['icon'],
-      'link' => $atts['link'],
+      'css_class' => isset($atts['class']) ?  $atts['class'] . ' icon icon-' . $atts['icon'] : 'icon icon-' . $atts['icon'],
+      'link' => isset($atts['link']) ? $atts['link'] : NULL,
+      'size' => isset($atts['size']) ? $atts['size'] . 'px' : NULL,
+
     ];
-  } else {
-    $args = [
-      'css_class' => 'icon icon-' . $atts['icon'],
-    ];
-  }
+  // } else {
+  //   $args = [
+  //     'css_class' => $atts['class'] . ' icon icon-' . $atts['icon'],
+  //     'size' => $atts['size'],
+  //   ];
+  // }
 
   return wp_get_svg( $path, $args );
   
@@ -72,8 +78,9 @@ function wp_get_svg( $path = '', $args = [] ) {
 
     $defaults = [
         'role' => 'image',
-        'css_class' => '',
-        'link' => '',
+        'css_class' => NULL,
+        'link' => NULL,
+        'size' => NULL,
     ];
 
     $args = wp_parse_args( $args, $defaults );
@@ -81,7 +88,8 @@ function wp_get_svg( $path = '', $args = [] ) {
     $role_attr = $args['role'];
     $css_class = $args['css_class'];
     $url = $args['link'];
- 
+    isset($args['size']) ? $size = 'width="'.$args['size'].'" height="'.$args['size'].'"' : $size = NULL;
+
     if ( is_array( $css_class ) ) {
         $css_class = implode( ' ', $css_class );
     } 
@@ -89,7 +97,7 @@ function wp_get_svg( $path = '', $args = [] ) {
     if ( file_exists( $path ) ) {
         $svg = file_get_contents( $path );
         $svg = preg_replace( '/(width|height)="[\d\.]+"/i', '', $svg );
-        $svg = str_replace( '<svg ', '<svg class="' . esc_attr( $css_class ) . '" role="' . esc_attr( $role_attr ) . '" ', $svg );
+        $svg = str_replace( '<svg ', '<svg '. $size .' class="' . esc_attr( $css_class ) . '" role="' . esc_attr( $role_attr ) . '"', $svg );
     }
 
     // Als we een SVG en een link hebben moet er een link tag omheen, dat doen we hier. Zo niet, dan geven we alleen de svg door.
