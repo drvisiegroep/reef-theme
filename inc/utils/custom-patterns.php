f7f42283ea2dfc009ff7912ed8fc4bb8ae62f3fd
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-01-21 22:39:57 +0100

    custom patterns onderverdeeld in aparte files

--- inc/utils/custom-patterns.php
+++ inc/utils/custom-patterns.php
@@ -17,72 +17,10 @@ register_block_pattern_category(
 
 
 function register_custom_block_patterns() {
-        register_block_pattern(
-            'custom/my_pattern',
-            array(
-                'title'         => __( 'Custom - M/T - USP - M/T', 'reef-theme' ),
-                'description'   => _x( 'Media/text - USP - Media/text', 'A media and textblock followed by a 3 column USP and another media and text block', 'reef-theme' ),
-				'categories'    => array( 'custom' ),
-                'keywords'      => array( 'custom', 'usp', 'media' ),
-//              'viewportWidth' => 800,
-                'content'       => '<!-- wp:media-text {"align":""} -->
-<div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
-<p class="has-large-font-size"></p>
-<!-- /wp:paragraph --></div></div>
-<!-- /wp:media-text -->
 
-<!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading -->
-<h2></h2>
-<!-- /wp:heading -->
+    include( get_template_directory() . '/inc/utils/custom-patterns/four-panel-media-text-dark-first.php' );
+    include( get_template_directory() . '/inc/utils/custom-patterns/usp-media-text.php' );
 
-<!-- wp:image -->
-<figure class="wp-block-image"><img alt=""/></figure>
-<!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading -->
-<h2></h2>
-<!-- /wp:heading -->
-
-<!-- wp:image -->
-<figure class="wp-block-image"><img alt=""/></figure>
-<!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading -->
-<h2></h2>
-<!-- /wp:heading -->
-
-<!-- wp:image -->
-<figure class="wp-block-image"><img alt=""/></figure>
-<!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
-
-<!-- wp:media-text {"align":"","mediaPosition":"right"} -->
-<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
-<p class="has-large-font-size"></p>
-<!-- /wp:paragraph --></div></div>
-<!-- /wp:media-text -->',
-
-            )
-        );
 }
 add_action( 'init', 'register_custom_block_patterns' );
 
