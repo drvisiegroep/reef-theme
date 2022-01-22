60c84b3f508b8bfcdb2475fc4ab544a4e1753a4b
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-01-22 11:20:28 +0100

    reusable block added

--- functions.php
+++ functions.php
@@ -37,12 +37,9 @@ include( get_template_directory() . '/inc/utils/custom-editor-options.php');
 include( get_template_directory() . '/inc/utils/custom-blocks.php');
 include( get_template_directory() . '/inc/utils/custom-fonts.php');
 
-
-
 //
 // Laad hier de styles en scripts.
 //
-
 function reef_scripts() {
     
     wp_enqueue_style( 'css-reef-theme-main', get_template_directory_uri() . '/assets/css/main.css', array(), filemtime( get_template_directory() . '/assets/css/main.css' ) );
@@ -56,7 +53,20 @@ function reef_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'reef_scripts' );
 
+
+
+/* Editor styles */
 add_editor_style( 'assets/css/main.css' );
+
+
+
+// Voeg reusable blocks toe aan admin menu
+function reef_reusable_blocks_admin_menu() {
+    add_menu_page( 'Reusable Blocks', 'Reusable Blocks', 'edit_posts', 'edit.php?post_type=wp_block', '', 'dashicons-editor-table', 22 );
+}
+add_action( 'admin_menu', 'reef_reusable_blocks_admin_menu' );
+
+
 
 // Admin bar alleen laten zien als user admin toegang of admin rechten heeft heeft
 if(current_user_can('manage_options') == 1) {
