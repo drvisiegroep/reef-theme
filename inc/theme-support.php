9839606f54fecbd7c3e500c3b9bb63a873ffbe5f
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-01-25 17:43:27 +0100

    if function exist voor child theme

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -16,71 +16,71 @@
 //
 if ( ! function_exists( 'reef_setup' ) ) :
 
-//
-// Deze functie wordt 'gehooked' in in de after_setup_theme hook. Deze gaat voor de init hook. 
-// Dit omdat de init hook voor bvepaalde eigenschappen (post-thumbnails bijvoorbeeld) te laat komt.
-// https://wordpress.stackexchange.com/questions/14797/difference-between-after-setup-theme-and-init-action-hooks
-// https://codex.wordpress.org/Plugin_API/Action_Reference
-//
-
-function reef_setup() {
-    
-    // Pad voor vertalingen
-    load_theme_textdomain( 'reef-theme', get_template_directory() . '/languages' );
-
-    // Editor Styles
-    add_theme_support( 'editor-styles' );
-
-    // Admin Bar Styling
-    add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
-
-    // Add default posts and comments RSS feed links to head.
-    // add_theme_support( 'automatic-feed-links' );
-
-    // Body open hook
-    add_theme_support( 'body-open' );
-
-    // Wordpress verzorgt de title tags i.p.v dat we ze er zelf inzetten.
-    add_theme_support( 'title-tag' );
-
     //
-    // Mogelijkheid voor plaatjes bij posts en pages
-    // https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+    // Deze functie wordt 'gehooked' in in de after_setup_theme hook. Deze gaat voor de init hook. 
+    // Dit omdat de init hook voor bvepaalde eigenschappen (post-thumbnails bijvoorbeeld) te laat komt.
+    // https://wordpress.stackexchange.com/questions/14797/difference-between-after-setup-theme-and-init-action-hooks
+    // https://codex.wordpress.org/Plugin_API/Action_Reference
     //
-    add_theme_support( 'post-thumbnails' );
 
-    //
-    // Menu's registreren
-    //
-    register_nav_menus( array(
-        'primary' => esc_html__( 'Primary Navigation Menu', 'reef-theme' ),
-        'secondary' => esc_html__( 'Secondary Navigation Menu', 'reef-theme' ),
-    ) );
+    function reef_setup() {
+        
+        // Pad voor vertalingen
+        load_theme_textdomain( 'reef-theme', get_template_directory() . '/languages' );
 
-    //
-    // HTML 5 support voor search, comments, gallerij en captions
-    //
-    add_theme_support( 'html5', array(
-        'search-form',
-        'comment-form',
-        'comment-list',
-        'gallery',
-        'caption',
-    ) );
+        // Editor Styles
+        add_theme_support( 'editor-styles' );
+        
+        // Admin Bar Styling
+        add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
 
-    // Responsive embeds
-    add_theme_support( 'responsive-embeds' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support( 'automatic-feed-links' );
 
-    // Wide Images
-    add_theme_support( 'align-wide' );
-    
-    //Block styles
-    // add_theme_support('wp-block-styles');
+        // Body open hook
+        add_theme_support( 'body-open' );
 
-    // Block templates
-    // add_theme_support('block-templates');
+        // Wordpress verzorgt de title tags i.p.v dat we ze er zelf inzetten.
+        add_theme_support( 'title-tag' );
 
-    }
+        //
+        // Mogelijkheid voor plaatjes bij posts en pages
+        // https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+        //
+        add_theme_support( 'post-thumbnails' );
+
+        //
+        // Menu's registreren
+        //
+        register_nav_menus( array(
+            'primary' => esc_html__( 'Primary Navigation Menu', 'reef-theme' ),
+            'secondary' => esc_html__( 'Secondary Navigation Menu', 'reef-theme' ),
+        ) );
+
+        //
+        // HTML 5 support voor search, comments, gallerij en captions
+        //
+        add_theme_support( 'html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ) );
+
+        // Responsive embeds
+        add_theme_support( 'responsive-embeds' );
+
+        // Wide Images
+        add_theme_support( 'align-wide' );
+        
+        //Block styles
+        add_theme_support('wp-block-styles');
+
+        // Block templates
+        add_theme_support('block-templates');
+
+        }
 endif;
 add_action( 'after_setup_theme', 'reef_setup' );
 ?>
--- inc/widgets.php
+++ inc/widgets.php
@@ -7,30 +7,33 @@
 // @since        1.0.0
 //
 
-add_action( 'widgets_init', 'reef_register_widgets' );
- 
-function reef_register_widgets() {
-    register_sidebar( array(
-        'name'              => esc_html__('primary-sidebar', 'reef-theme'),
-        'id'                => 'primary-sidebar',
-        'before_widget'     => '<div id="%1$s" class="widget %2$s">',
-        'after_widget'      => '</div>',
-        )
-    );
-    register_sidebar( array(
-        'name'              => esc_html__('footer-1', 'reef-theme'),
-        'id'                => 'footer-1',
-        'before_widget'     => '<div id="%1$s" class="widget %2$s">',
-        'after_widget'      => '</div>',
-        )
-    );
-    // register_sidebar( array(
-    //     'name'              => esc_html__('footer-2', 'reef-theme'),
-    //     'id'                => 'footer-2',
-    //     'before_widget'     => '<div id="%1$s" class="widget %2$s">',
-    //     'after_widget'      => '</div>',
-    //     )
-    // );
 
-}
+if ( ! function_exists( 'reef_register_widgets' ) ) :
+    function reef_register_widgets() {
+        register_sidebar( array(
+            'name'              => esc_html__('primary-sidebar', 'reef-theme'),
+            'id'                => 'primary-sidebar',
+            'before_widget'     => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'      => '</div>',
+            )
+        );
+        register_sidebar( array(
+            'name'              => esc_html__('footer-1', 'reef-theme'),
+            'id'                => 'footer-1',
+            'before_widget'     => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'      => '</div>',
+            )
+        );
+        register_sidebar( array(
+            'name'              => esc_html__('footer-2', 'reef-theme'),
+            'id'                => 'footer-2',
+            'before_widget'     => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'      => '</div>',
+            )
+        );
+
+    }
+endif;
+
+add_action( 'widgets_init', 'reef_register_widgets' );
 ?>
