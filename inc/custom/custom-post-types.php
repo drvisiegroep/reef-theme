e8099b02b86b9691ff0f45f40a25e7c63bd5100a
daniel <daniel@websiteaanbieder.nl>
2022-01-27 16:57:58 +0100

    cpt boeken uit

--- inc/custom/custom-post-types.php
+++ inc/custom/custom-post-types.php
@@ -15,20 +15,20 @@
 // Registreer je CPT's hier.
 // TODO: voorbeeld CPT en / of URL met CPT generator
 
-add_action('init', function() {
-	register_post_type('boeken', [
-		'label' => __('Boeken', 'reef-theme'),
-		'public' => true,
-		'show_in_rest' => true,
-       	'supports' => array('editor','title', 'excerpt', 'thumbnail'),
-		'template' => [			
-			['core/cover', ['align' => 'full', 'overlayColor' => 'primary'], [
-				['core/heading', ['align' => 'center', 'placeholder' => __('Uw titel', 'reef-theme')]],
-				['core/paragraph', ['align' => 'center', 'placeholder' => __('Uw tekst hier', 'reef-theme')]],
-				['core/button', ['align' => 'center']]
-			]],
-		],
-		'template_lock' => 'all',
-	]);
-});
+// add_action('init', function() {
+// 	register_post_type('boeken', [
+// 		'label' => __('Boeken', 'reef-theme'),
+// 		'public' => true,
+// 		'show_in_rest' => true,
+//        	'supports' => array('editor','title', 'excerpt', 'thumbnail'),
+// 		'template' => [			
+// 			['core/cover', ['align' => 'full', 'overlayColor' => 'primary'], [
+// 				['core/heading', ['align' => 'center', 'placeholder' => __('Uw titel', 'reef-theme')]],
+// 				['core/paragraph', ['align' => 'center', 'placeholder' => __('Uw tekst hier', 'reef-theme')]],
+// 				['core/button', ['align' => 'center']]
+// 			]],
+// 		],
+// 		'template_lock' => 'all',
+// 	]);
+// });
 ?>
