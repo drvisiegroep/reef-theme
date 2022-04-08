ab570eed3dade6cca9d589ba3656de10c2fb75c2
daniel <daniel@websiteaanbieder.nl>
2022-04-08 13:38:06 +0200

    slick verwijderd

--- inc/custom/custom-enqueue.php
+++ inc/custom/custom-enqueue.php
@@ -7,17 +7,7 @@
 // @since        1.0.0
 //
 
-// TODO: custom laden als child theme niet geladen is.
-
 // Lage prio voor check op child-theme-css
-function slick() {
-    wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/assets/js/slick/slick.css', array() );
-    wp_enqueue_style( 'slick-theme-css', get_template_directory_uri() . '/assets/js/slick/slick-theme.css', array() );
-    wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/assets/js/slick/slick.js', array( 'jquery' ), '1.5.3', true );
-    wp_enqueue_script( 'slick-init',  get_template_directory_uri() . '/assets/js/slick/slick-init.js', array( 'slick-js' ), '1.0.0', true );
-}
-// add_action( 'wp_enqueue_scripts', 'slick',50);
-
 function reef_scripts() {
 
    
@@ -36,11 +26,11 @@ function reef_scripts() {
         wp_enqueue_script( 'jquery' );
     }
 
-
 }
 add_action( 'wp_enqueue_scripts', 'reef_scripts',99);
 
 /* Editor styles */
+/* Prio moet hoger zijn dan van de child */
 function reef_editor_styles () {
     add_editor_style( 'assets/css/main.css' );
 }
